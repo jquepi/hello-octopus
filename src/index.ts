69f0634d6f5f9eceb8dbe8963f463ee5c4fa4c9e
Michael Richardson <michael.richardson@octopus.com>
2022-01-14 17:09:59 +1000

    Added static content

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import path from "path";
+
 const app = express();
 const port = 8080; 
+const staticPath = path.join(__dirname, 'static');
+
+app.use('/', express.static(staticPath));
 
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+    res.sendFile(path.join(staticPath, 'index.html'));
 } );
 
 app.listen( port, () => {
